d4a9ceefd6c4469c13d84e7b313182a0a461d9fa
parnia <parniamaroofi@gmail.com>
2023-10-15 23:42:23 +0330

    ADD custom icons

---
+++ src/assets/customIcons.js
@@ -0,0 +1,22 @@
+import { h } from 'vue';
+import bed from '@/components/icons/Bed.vue';
+import home from '@/components/icons/Home.vue';
+import bell from '@/components/icons/Bell.vue';
+import bath from '@/components/icons/Bath.vue';
+import parking from '@/components/icons/Parking.vue';
+import location from '@/components/icons/Location.vue';
+
+const iconsComponent = {
+  bed,
+  home,
+  bell,
+  bath,
+  parking,
+  location,
+};
+
+const customIcons = {
+  component: (props) => h(iconsComponent[props.icon]),
+};
+
+export { customIcons /* aliases */ };
--- src/main.js
+++ src/main.js
@@ -6,11 +6,24 @@ import router from './router/index.js';
 // Vuetify
 import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css';
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { customIcons } from '@/assets/customIcons.js';
 
 const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'light',
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+      svg: customIcons,
+    },
+  },
   components,
   directives,
 });
